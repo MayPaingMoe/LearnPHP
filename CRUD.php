10967b40bde256b2c678ed4dc2d370a03578158a
MayPaingMoe <miem20674@gmail.com>
2024-06-16 13:39:54 +0630

    [CRUD]create & read

--- CRUD.php
+++ CRUD.php
@@ -1,35 +1,79 @@
 <?php
+ echo "<pre/>";
 
-try{
+//create();
+read ();
+function create(){
+    try{
         // connect to database
         $pdo=new PDO("mysql:host=localhost;dbname=school","root");
     
         echo "Database connect Successful";
 
 // create data
-    $statement=$pdo->query("INSERT INTO `STUDENT`(`name`,`email`,`gender`,`dob`,`age`)VALUES
-('John Doe','john@gmail.com','male','1998-01-01','27');");
+// use query in pdo cuz connect database
+    $statement=$pdo->query("INSERT INTO `STUDENT`(`name`,`email`,`gender`,`dob`,`age`)
+    VALUES('John Doe','john@gmail.com','male','1998-01-01','27');");
 
 echo "<br>";
 
 echo "Data insert Complete";
 // var_dump($statement);
+}catch(PDOException $e){
 
- function read(){
-    $pdo=new PDO("mysql:host=localhost;dbname=school","root");
-    $statement=$pdo->query("Select * from student");
-    echo "<pre/>";
-    $student =$statement->fetchAll(PDO::FETCH_ASSOC);
+   
 
-    foreach($student as $student){
-        echo $student["name"];
-    }
+    var_dump($e->getMessage());
+   
+}catch(Exception $e){
+
+    var_dump(($e->getMessage()));
+}
 }
 
-    }catch(PDOException $e){
-        echo "<pre/>";
-        var_dump($e->getMessage());
+ function read(){
+    try{
+        $pdo=new PDO("mysql:host=localhost;dbname=school","root");
+
+        $statement=$pdo->query("Select * from `student`");
+    
        
-    }catch(Exception $e){
-        var_dump(($e->getMessage()));
-    }
+    // select all
+
+    // fetch just one data,fetchall  all data
+    // fetch-assoc mean use associative array
+
+        // $students =$statement->fetchAll(PDO::FETCH_ASSOC);
+    
+        // // foreach satement vari and as new vari 
+        // foreach($students as $student){
+        //     echo "<pre/>";
+        //catch with array
+        //      echo $student["name"]."-". $student["email"];
+    
+        // }
+
+        // fetch-obj return as obj
+
+        $students=$statement->fetchAll(PDO::FETCH_OBJ);
+
+       
+       foreach($students as $student){
+        echo "<pre/>";
+        // catch with obj
+        echo $student ->name. "-". $student ->email;
+       }
+
+}catch(PDOException $e){
+    echo "<pre/>";
+    var_dump($e->getMessage());
+   
+}catch(Exception $e){
+
+    var_dump(($e->getMessage()));
+}
+}
+
+   
+
+   
